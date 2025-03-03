b6be6d55e1ef12c1e392ded44fe3324545528bc3
Talha Ali <tlha2422@gmail.com>
2025-03-03 12:14:28 -0600

    removing rss parser to see if it fixes it

--- app/podcasts/page.tsx
+++ app/podcasts/page.tsx
@@ -5,15 +5,15 @@ import styles from "./page.module.css";
 import Podcast from "@/components/embeddedPodcast";
 import FeaturedPodcasts from "@/components/featuredPodcasts";
 import PaginatedScroll from "@/components/PaginatedScroll";
-import parser from "../helpers/parseFeed";
+// import parser from "../helpers/parseFeed";
 
 export default async function PodcastsPage() {
   const [paginated, lastDate] = await fetchHelper.getPaginatedStories("podcasts");
   const feat_ads = await adsHelper.getFeaturedAds(); 
   const page_ads = await adsHelper.getPageAds();
-  const parsed = await parser();
+  // const parsed = await parser();
   
-  console.log('parse', parsed);
+  // console.log('parse', parsed);
 
   let ads = feat_ads.concat(page_ads)
 
@@ -21,10 +21,10 @@ export default async function PodcastsPage() {
 
   return (
     <main>
-      {
+      {/* {
         (parsed.enclosure && parsed.enclosure.url) &&
           <Podcast url={parsed.enclosure.url} description={parsed.contentSnippet} />
-      }
+      } */}
       <FeaturedPodcasts />
       <PaginatedScroll stories={paginated} ads={ads.splice(0, 4)} filter="podcasts" lastDate={lastDate}/>
     </main>
